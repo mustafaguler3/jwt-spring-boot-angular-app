abf57691beafe1f94835aafc095420a4e2632920
mustafaguler3 <mustafa.glr@hotmail.com>
2023-10-09 14:46:39 +0300

    user repository added

---
+++ src/main/java/com/example/demo/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.repositories;
+
+import com.example.demo.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    User findUserByUsername(String username);
+
+    User findUserByEmail(String email);
+}
