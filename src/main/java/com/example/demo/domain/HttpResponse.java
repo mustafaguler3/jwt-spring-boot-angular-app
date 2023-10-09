0aca3812c10f5bdad2a39a0bda86d0b9eb8f6e8e
mustafaguler3 <mustafa.glr@hotmail.com>
2023-10-09 13:20:32 +0300

    user domain and custom response class added

---
+++ src/main/java/com/example/demo/domain/HttpResponse.java
@@ -0,0 +1,18 @@
+package com.example.demo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class HttpResponse {
+
+    private int statusCode;
+    private HttpStatus httpStatus;
+    private String reason;
+    private String message;
+
+}
---
+++ src/main/java/com/example/demo/domain/User.java
@@ -0,0 +1,54 @@
+package com.example.demo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class User implements Serializable {
+
+    private static final long serialVersionUID = -2731598327208972274L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String userId;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private String email;
+    private String imageUrl;
+    private Date lastLoginDate;
+    private Date lastLoginDateDisplay;
+    private Date joinDate;
+    private String[] roles;
+    private String[] authorities;
+    private boolean isActive;
+    private boolean isLocked;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
