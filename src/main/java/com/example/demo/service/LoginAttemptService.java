5170e714b175106ce04980ef48a7593376036fb6
mustafaguler3 <mustafa.glr@hotmail.com>
2023-10-12 12:11:31 +0300

    bruce force attack cache added

---
+++ src/main/java/com/example/demo/service/LoginAttemptService.java
@@ -0,0 +1,61 @@
+package com.example.demo.service;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class LoginAttemptService {
+    private static final int MAXIMUM_NUMBER_OF_ATTEMPT = 5;
+    private static final int ATTEMPT_INCREMENT = 1;
+
+    private LoadingCache<String, Integer> loginAttemptCache;
+
+    public LoginAttemptService() {
+        super();
+        loginAttemptCache = CacheBuilder.newBuilder().expireAfterWrite(15, TimeUnit.MINUTES).maximumSize(100).build(new CacheLoader<String, Integer>() {
+            @Override
+            public Integer load(String s) throws Exception {
+                return 0;
+            }
+        });
+    }
+
+    public void evictUserFromLoginAttemptCache(String username){
+        loginAttemptCache.invalidate(username);
+    }
+
+    public void addUserToLoginAttempCache(String username) throws ExecutionException {
+        int attempts = 0;
+        attempts = ATTEMPT_INCREMENT + loginAttemptCache.get(username);
+        loginAttemptCache.put(username,attempts);
+    }
+
+    public boolean hasExceededMaxAttempts(String username) throws ExecutionException {
+        return loginAttemptCache.get(username) >= MAXIMUM_NUMBER_OF_ATTEMPT;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
